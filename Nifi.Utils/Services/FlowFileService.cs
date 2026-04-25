674b1565c21614ab2537af1a9c124c3b5a9b0420
linus-berg <linus@fenix.me.uk>
2026-04-25 11:37:17 +0200

    Updated if statements.

--- Nifi.Utils/Services/FlowFileService.cs
+++ Nifi.Utils/Services/FlowFileService.cs
@@ -73,12 +73,15 @@ namespace Nifi.Utils.Services {
       while (!ct.IsCancellationRequested) {
         byte[] header = new byte[S_MAGIC_HEADER_V3_.Length];
         int read = await input_stream.ReadAsync(header, 0, header.Length, ct);
-        if (read == 0)
+        if (read == 0) {
           yield break;
-        if (read < header.Length || !header.SequenceEqual(S_MAGIC_HEADER_V3_))
+        }
+
+        if (read < header.Length || !header.SequenceEqual(S_MAGIC_HEADER_V3_)) {
           throw new InvalidDataException(
             "Invalid NiFi FlowFile V3 Magic Header"
           );
+        }
 
         NifiPackage package = new NifiPackage();
         int attr_count = await ReadFieldLengthAsync(input_stream, ct);
@@ -96,8 +99,9 @@ namespace Nifi.Utils.Services {
         while (remaining > 0) {
           int to_read = (int)Math.Min(buffer.Length, remaining);
           int bytes_read = await input_stream.ReadAsync(buffer, 0, to_read, ct);
-          if (bytes_read == 0)
+          if (bytes_read == 0) {
             throw new EndOfStreamException();
+          }
           await ms.WriteAsync(buffer, 0, bytes_read, ct);
           remaining -= bytes_read;
         }
@@ -121,7 +125,7 @@ namespace Nifi.Utils.Services {
 
     public async Task WriteFlowFileV1Async(NifiPackage package,
                                            Stream output_stream) {
-      using TarWriter writer = new TarWriter(
+      await using TarWriter writer = new TarWriter(
         output_stream,
         TarEntryFormat.Ustar,
         leaveOpen: true
@@ -161,8 +165,9 @@ namespace Nifi.Utils.Services {
       int read = 0;
       while (read < length) {
         int r = await stream.ReadAsync(bytes, read, length - read, ct);
-        if (r == 0)
+        if (r == 0) {
           throw new EndOfStreamException();
+        }
         read += r;
       }
 
@@ -185,14 +190,17 @@ namespace Nifi.Utils.Services {
     private async Task<int> ReadFieldLengthAsync(
       Stream stream, CancellationToken ct) {
       byte[] bytes = new byte[2];
-      if (await stream.ReadAsync(bytes, 0, 2, ct) < 2)
+      if (await stream.ReadAsync(bytes, 0, 2, ct) < 2) {
         throw new EndOfStreamException();
+      }
       ushort length = BinaryPrimitives.ReadUInt16BigEndian(bytes);
-      if (length < 0xFFFF)
+      if (length < 0xFFFF) {
         return length;
+      }
       byte[] full_bytes = new byte[4];
-      if (await stream.ReadAsync(full_bytes, 0, 4, ct) < 4)
+      if (await stream.ReadAsync(full_bytes, 0, 4, ct) < 4) {
         throw new EndOfStreamException();
+      }
       return BinaryPrimitives.ReadInt32BigEndian(full_bytes);
     }
 
@@ -205,8 +213,9 @@ namespace Nifi.Utils.Services {
     private async Task<long>
       ReadInt64Async(Stream stream, CancellationToken ct) {
       byte[] bytes = new byte[8];
-      if (await stream.ReadAsync(bytes, 0, 8, ct) < 8)
+      if (await stream.ReadAsync(bytes, 0, 8, ct) < 8) {
         throw new EndOfStreamException();
+      }
       return BinaryPrimitives.ReadInt64BigEndian(bytes);
     }
 
